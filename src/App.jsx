e7301d8d4e77357df87698883b0ab5bfd90754f1
mdot2x3 <mdot2x3@gmail.com>
2026-05-31 18:05:26 -0700

    feat: add new pages, Navbar component
    add components and pages directories to src.
    
    create a basic placeholder Navbar component.
    
    organize layout of App component which will act as
    the home page.
    
    create Shop and Cart pages.
    
    update README.md.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <main className="app-container">Hello world</main>;
+  return (
+    <div className="app">
+      <header>
+        <nav className="nav">
+          <Navbar />
+        </nav>
+      </header>
+      <main className="main">
+        <h1>Welcome to the Home Page.</h1>
+      </main>
+      <footer className="footer">footer here</footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Shop</li>
+      <li>Cart</li>
+    </ul>
+  );
+};
+
+export default Navbar;
