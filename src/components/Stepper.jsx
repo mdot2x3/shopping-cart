f947d5eb9b2a9f4a8c054a76ce8dae657eb9259e
mdot2x3 <mdot2x3@gmail.com>
2026-06-06 16:32:07 -0700

    feat: allow user input into input field
    modify Stepper to allow users to manually type in
    how many items they want to buy and then increment
    from that value.
    
    add handleInputChange that collects the user input
    value, and sets state for empty or valid number
    input values.
    
    update handleStepperClick to ensure compatibility
    with possible empty input fields.

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -2,16 +2,34 @@ import styles from "./Stepper.module.css";
 
 const Stepper = ({ quantity, setQuantity }) => {
   const handleStepperClick = (action) => {
-    if (action == "increment") {
-      setQuantity(quantity + 1);
-    } else if (action == "decrement") {
-      setQuantity(Math.max(0, quantity - 1));
+    // use current in case user clears input and then presses a button
+    // use built-in JS function Number to convert value into JS number
+    const current = Number(quantity) || 0;
+    if (action === "increment") {
+      setQuantity(current + 1);
+    } else if (action === "decrement") {
+      setQuantity(Math.max(0, current - 1));
     }
   };
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "") {
+      setQuantity("");
+      return;
+    }
+
+    const number = Number(value);
+    if (!Number.isNaN(number) && number >= 0) {
+      setQuantity(number);
+    }
+  };
+
   return (
     <div className={styles.stepper}>
       <button onClick={() => handleStepperClick("decrement")}>-</button>
-      <input type="text" value={quantity} />
+      <input type="text" value={quantity} onChange={handleInputChange} />
       <button onClick={() => handleStepperClick("increment")}>+</button>
     </div>
   );
