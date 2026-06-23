ae29542676c64175a378ebfdd0a1ad6e575d6f41
mdot2x3 <mdot2x3@gmail.com>
2026-06-22 19:27:35 -0700

    style: add logo, restyle navBar
    remove centering from header in the App css
    module.
    
    add new divs to the navBar that breaks it into a
    left, center, and right part with the logo placed
    in the left div and ul links placed in the center,
    with the right existing simply for placement
    centering.
    
    add styling to the Navbar css module to set up a
    grid with the three divs, affixing the logo to the
    top left of the page and centering the nav links,
    also change the nav links from a button appearance
    to just plain text with coloring and underling
    effects when active or on hover, and lastly have
    the logo shift above the nav links when the window
    is small using media queries.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,55 @@
 import styles from "./Navbar.module.css";
-import { NavLink } from "react-router";
+import { Link, NavLink } from "react-router";
 
 const Navbar = ({ cartCount }) => {
   return (
-    <ul className={styles.navbarUl}>
-      <li>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/shop"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Shop
-        </NavLink>
-      </li>
-      <li className={styles.cartTab}>
-        <NavLink
-          to="/cart"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Cart
-        </NavLink>
-        {cartCount > 0 && <span className={styles.cartBadge}>{cartCount}</span>}
-      </li>
-    </ul>
+    <div className={styles.navbar}>
+      <div className={styles.navLeft}>
+        <Link to="/" className={styles.logo}>
+          Proto
+        </Link>
+      </div>
+
+      <div className={styles.navCenter}>
+        <ul className={styles.navbarUl}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/shop"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              Shop
+            </NavLink>
+          </li>
+          <li className={styles.cartTab}>
+            <NavLink
+              to="/cart"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              Cart
+            </NavLink>
+            {cartCount > 0 && (
+              <span className={styles.cartBadge}>{cartCount}</span>
+            )}
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.navRight} />
+    </div>
   );
 };
 
